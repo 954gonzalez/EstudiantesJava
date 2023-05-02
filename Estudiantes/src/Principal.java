97476e04405d55cccedf3fd7df528608066f0660
954gonzalez <jagonzalez954@soy.sena.edu.co>
2023-05-02 17:38:46 -0500

    EstudiantesJava

---
+++ Estudiantes/src/Principal.java
@@ -0,0 +1,11 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Procesos opera;
+		opera=new Procesos();
+         opera.iniciar();
+	}
+
+}
